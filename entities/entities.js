4c95eebc854f66891fd6432c19aa4cdb654123cc
whyshurik <thesasha010@gmail.com>
2022-10-23 23:03:29 +0300

    Added users creation

---
+++ entities/entities.js
@@ -0,0 +1,7 @@
+const users = new Set();
+const categories = new Set();
+const records = new Set();
+
+module.exports={
+    users,categories,records
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const entities = require('../entities/entities.js')
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/adduser', function(req, res, next) {
+  entities.users.add(req.body)
   res.send('respond with a resource');
 });
+router.get('/', function(req, res, next) {
+  console.log(entities.users)
 
+  res.send(JSON.stringify([...entities.users]));
+});
 module.exports = router;
